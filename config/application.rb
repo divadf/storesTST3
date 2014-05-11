231ed410f1a8bd690567f404fe88eaf05c9bdb50
divadf <divad.figueroa@gmail.com>
2014-05-11 17:30:34 -0400

    funcional carousel

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module StoresTST
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
-    # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
